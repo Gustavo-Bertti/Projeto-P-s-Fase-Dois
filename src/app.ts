15dee4523e076b648ea7ffb8b13e824795844f64
Gustavo-Bertti <bertinhobertti@hotmail.com>
2025-07-23 16:59:56 -0300

    Implementações

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
 import { postagemRoutes } from '@/http/controllers/postagem/routes'
+import { globalErrorHandler } from '@/utils/global-error-handler'
 
 export const app = fastify()
 
 app.register(postagemRoutes)
+app.setErrorHandler(globalErrorHandler);
