dc2bd88817498b3eb13147670c643fa1af2fab97
Gustavo-Bertti <bertinhobertti@hotmail.com>
2025-08-01 15:28:33 -0300

    Ajuste

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ app
     port: env.PORT,
   })
   .then(() => {
-    console.log('Servidor está rodando no link http://localhost:3000')
+    console.log('Servidor está rodando no link http://localhost:' + env.PORT)
   })
