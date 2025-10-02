8fc7a0beb30f22c6f8a9a987762fa51b857e6626
Gustavo-Bertti <bertinhobertti@hotmail.com>
2025-10-02 16:30:39 -0300

    Alteração no token

--- src/use-cases/login-usuario.ts
+++ src/use-cases/login-usuario.ts
@@ -3,7 +3,7 @@ import { IUsuarioRepository } from "@/repositories/usuario.repository.interface"
 export class LoginUsuarioUseCase {
     constructor(private usuarioRepository: IUsuarioRepository) { }
 
-    async handler(email: string, senha: string): Promise<string | null> {
+    async handler(email: string, senha: string): Promise<{ idUsuario?: string, idTipo?: number } | null> {
         return await this.usuarioRepository.login(email, senha);
     }
 }
